6eccd8e2043d8ddfb093e93403d9a583b654b3af
James Starner <Jstarner3@gmail.com>
2023-09-18 08:02:45 -0600

    Edit settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #....
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # James Starner
+    'portfolio_app',
+]
+
+#Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
